be1cad530ddbeed930525eddb66d6fab7f97ea06
tinaun <tinagma@gmail.com>
2018-12-01 22:58:54 -0500

    day 1 part 2

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,19 @@ fn main() {
     let demo = include_str!("../demo.txt");
     let input = include_str!("../input.txt");
     let mut count = 0;
+    let mut repeats = Set::new();
+    repeats.insert(count);
 
-    for c in input.lines() {
+    for c in input.lines().cycle() {
         let i: i32 = c.parse().unwrap();
         count += i;
+        if repeats.contains(&count) {
+            dbg!(("p2", count));
+            break;
+        }
+        repeats.insert(count);
     }
     
 
-    println!("p1: {}", count);
+    //println!("p1: {}", count);
 }
