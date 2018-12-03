36bbc97c37109c32b4974a27d5359a9a641270b5
tinaun <tinagma@gmail.com>
2018-12-02 22:40:20 -0500

    aoc day 2, both parts

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,57 @@ fn main() {
     let demo = include_str!("../demo.txt");
     let input = include_str!("../input.txt");
     let mut count = 0;
-    let mut repeats = Set::new();
-    repeats.insert(count);
 
-    for c in input.lines().cycle() {
-        let i: i32 = c.parse().unwrap();
-        count += i;
-        if repeats.contains(&count) {
-            dbg!(("p2", count));
-            break;
+    let mut twice = 0;
+    let mut thrice = 0;
+    for l in input.lines() {
+        let mut t = Map::new();
+        for x in l.chars() {
+            *t.entry(x).or_insert(0) += 1;
         }
-        repeats.insert(count);
-    }
-    
 
-    //println!("p1: {}", count);
+        for (k, v) in &t {
+            if *v == 2 {
+                twice += 1;
+                break;
+            }
+        }
+
+        for (k, v) in &t {
+            if *v == 3 {
+                thrice += 1;
+                break;
+            }
+        }
+    }
+
+    println!("p1: {}", twice * thrice);
+
+    fn check(a: &str, b: &str) -> Option<String> {
+        if a == b {
+            return None;
+        }
+        let mut once = None;
+        let mut res = String::new();
+        for (i, (x, y)) in a.chars().zip(b.chars()).enumerate() {
+            if x != y && once.is_none() {
+                once = Some(());
+            } else if x != y {
+                return None;
+            } else {
+                res.push(x);
+            }
+        }
+
+        once.map(|_| res)
+    }
+
+
+    for (x, y) in input.lines().tuple_combinations() {
+        if let Some(ans) = check(&x, y) {
+            println!("p2: {}", ans);
+        }
+    }
+
+
 }
--- src/prelude.rs
+++ src/prelude.rs
@@ -7,3 +7,7 @@ pub use std::collections::HashMap as Map;
 
 pub use std::io;
 pub use std::fmt::{self, Display};
+
+pub use serde_derive::{Serialize, Deserialize};
+
+pub use serde_scan::from_str as scan;
