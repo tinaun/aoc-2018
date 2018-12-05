0f7338b2a4fb7a4911675d857d71b83887a956e0
tinaun <tinagma@gmail.com>
2018-12-05 11:38:16 -0500

    day 5 updates

--- src/main.rs
+++ src/main.rs
@@ -5,34 +5,35 @@ mod prelude;
 use self::prelude::*;
 
 fn compare_case(b: u8, a: u8) -> bool {
-    a == b || ((a as i32) - (b as i32)).abs() == 32
+    (a | 32) == (b | 32)
 }
 
 fn p1(next: &[u8]) -> usize {
     let mut next = next.to_vec();
+    let mut last = next.len();
 
     loop {
-        //println!("{}", String::from_utf8_lossy(&next));
-        let mut new = vec![];
-        let mut i = 0;
-            while i < next.len() - 1 {
-                if ((next[i] as i32) - (next[i+1] as i32)).abs() == 32 {
-                    i += 1;
-                } else {
-                    new.push(next[i]);
-                }
-                if i == next.len() - 2 {
-                    new.push(next[i+1]);
-                }
-                i += 1;
-            }
-
-        //println!("{}", next.len());
-        if next == new {
-            break new.len()
-        }
         
-        next = new;
+        let mut skip = false;
+        next = next.into_iter().chain(std::iter::once(0)).tuple_windows().filter_map(move |(a, b)| {
+            if skip {
+                skip = false;
+                None
+            } else if (a != b) && (a | 32) == (b | 32) {
+                skip = true;
+                None
+            } else {
+                Some(a)
+            }
+        }).collect();
+
+        
+        if last == next.len() {
+            break next.len()
+        } else {
+            last = next.len();
+        }
+
     }
 }
 
