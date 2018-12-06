3ad10d5ad4d017e85297638422c5fadad8e46d93
tinaun <tinagma@gmail.com>
2018-12-06 00:34:38 -0500

    day 6!

--- src/main.rs
+++ src/main.rs
@@ -4,52 +4,65 @@
 mod prelude;
 use self::prelude::*;
 
-fn compare_case(b: u8, a: u8) -> bool {
-    (a | 32) == (b | 32)
-}
-
-fn p1(next: &[u8]) -> usize {
-    let mut next = next.to_vec();
-    let mut last = next.len();
-
-    loop {
-        
-        let mut skip = false;
-        next = next.into_iter().chain(std::iter::once(0)).tuple_windows().filter_map(move |(a, b)| {
-            if skip {
-                skip = false;
-                None
-            } else if (a != b) && (a | 32) == (b | 32) {
-                skip = true;
-                None
-            } else {
-                Some(a)
-            }
-        }).collect();
-
-        
-        if last == next.len() {
-            break next.len()
-        } else {
-            last = next.len();
-        }
-
-    }
-}
 
 fn main() {
-    let demo = "abBA";//include_str!("../demo.txt");
+    let demo = include_str!("../demo.txt");
     let input = include_str!("../input.txt");
+
+    let mut map = Map::new();
+    
+    for l in input.lines() {
+        let loc: (i32, i32) = s!("{}, {}" <- l).unwrap();
+        map.insert(loc, 0);
+    }
+
+    let mut infinites = Set::<(i32, i32)>::new();
+    let edges = [-400, 1000];
+
+    for x in -400..=1000 {
+        for y in -400..=1000 {
+            let mut min_dist = 10_000_000;
+            let mut closest = None;
+            for &(a, b) in map.keys() {
+                let dist = (a - x).abs() + (b - y).abs();
+                if dist < min_dist {
+                    min_dist = dist;
+                    closest = Some((a, b));
+                } else if dist == min_dist {
+                    closest = None;
+                }
+            }
+
+            if closest.is_some() {
+                let closest = closest.unwrap();
+                *map.entry(closest).or_insert(0) += 1;
+                if edges.contains(&x) || edges.contains(&y) {
+                    infinites.insert(closest);
+                }
+            }
+        } 
+    }
+
+    let max = map.iter().filter(|x| !infinites.contains(&x.0)).max_by_key(|&(a, b)| b);
+
+    dbg!(max);
+
+    let limit = 10_000;
+    let heuristic = 10_000 / map.len();
     let mut count = 0;
-    
-    let mut next = input.trim().to_string().into_bytes();
-    dbg!(p1(&next));
 
-    let p2 = (b'a'..=b'z').map(|ch| {
-        let next: Vec<_> = next.clone().into_iter().filter(|c| !compare_case(ch, *c)).collect();
+    for x in -100..=600 {
+        for y in -100..=600 {
+            let mut sum = 0;
+            for &(a, b) in map.keys() {
+                sum += (a - x).abs() + (b - y).abs();
+            }
 
-        p1(&next)
-    }).min();
-    
-    dbg!(p2);
+            if sum < limit {
+                count += 1;
+            }
+        }
+    }
+
+    dbg!(count);
 }
