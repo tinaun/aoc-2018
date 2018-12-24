e0ed3ad39b18823cbc98201f246ba50305bd86e8
tinaun <tinagma@gmail.com>
2018-12-23 23:59:10 -0500

    day 23, both parts

--- src/lib.rs
+++ src/lib.rs
@@ -3,55 +3,28 @@
 mod prelude;
 use self::prelude::*;
 
-const DEPTH: usize = 8787;
-const TARGET: (usize, usize) = (10, 725);
 
-fn geo_index(x: usize, y: usize, world: &mut Map<(usize, usize), usize>) -> usize {
-    if let Some(&value) = world.get(&(x,y)) {
-        return value;
+
+#[derive(Debug, Copy, Clone, Deserialize, PartialEq)]
+pub struct Nanobot {
+    pos: (i64, i64, i64),
+    range: i64,
+}
+
+impl Nanobot {
+    fn in_range(&self, other: &Self) -> bool {
+        self.in_range_pt(other.pos)
     }
-    
-    let value = if (x, y) == (0, 0) {
-        0
-    } else if (x, y) == TARGET {
-        0
-    } else if x == 0 {
-        y * 48271
-    } else if y == 0 {
-        x * 16807
-    } else {
-        erosion_level(x-1, y, world) * erosion_level(x, y - 1, world)
-    };
 
-    world.insert((x, y), value);
+    fn in_range_pt(&self, pos: (i64, i64, i64)) -> bool {
+        let dist = (self.pos.0 - pos.0).abs()
+        + (self.pos.1 - pos.1).abs() 
+        + (self.pos.2 - pos.2).abs();
+        //let dist = (dist as f64).sqrt();
 
-    value
+        self.range >= dist
+    }
 }
-
-fn erosion_level(x: usize, y: usize, world: &mut Map<(usize, usize), usize>) -> usize {
-    (geo_index(x, y, world) + DEPTH) % 20183
-}
-
-#[derive(Copy, Clone, Debug, PartialEq)]
-enum Cave {
-    Rocky,
-    Wet,
-    Narrow,
-}
-
-#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
-enum Tool {
-    Gear,
-    Torch,
-    None,
-}
-
-fn man_dist(a: (usize, usize), b: (usize, usize)) -> usize {
-    let man = (a.0 as isize - b.0 as isize).abs() + (a.1 as isize - b.1 as isize).abs();
-
-    man as usize
-}
-
 
 pub fn advent() -> (impl Debug, impl Debug) {
     let demo = include_str!("../demo.txt");
@@ -59,90 +32,54 @@ pub fn advent() -> (impl Debug, impl Debug) {
     let mut p1 = 0;
     let mut p2 = 0;
 
-    let mut world = Map::new();
+    let mut bots = vec![];
 
-    for y in 0..=TARGET.1 {
-        for x in 0..=TARGET.0 {
-            let ero = (geo_index(x, y, &mut world) + DEPTH) % 20183;
-            p1 += ero % 3;
+    for i in input.lines() {
+        let bot: Nanobot = s!("pos=<{},{},{}>, r={}" <- i).unwrap();
+        bots.push(bot);
+    }
+
+    let max = bots.iter().max_by_key(|b| b.range).unwrap();
+    
+    let mut total = 0;
+    for other in &bots {
+        if max.in_range(other) {
+            total += 1;
         }
     }
 
-    let mut cave_system = vec![Cave::Rocky; 1000 * 1000];
-
-    for y in 0..1000 {
-        for x in 0..1000 {
-            let ero = (geo_index(x, y, &mut world) + DEPTH) % 20183;
-            cave_system[y * 1000 + x] = match ero % 3 {
-                1 => Cave::Wet,
-                2 => Cave::Narrow,
-                _ => {
-                    continue;
-                },
-            };
-        }
-    }
-    println!("starting path");
-    let mut visited = Map::new();
-    let mut dist = Map::new();
-    dist.insert((0, 0, Tool::Torch), 0);
-
-    let mut current = (0, 0, Tool::Torch);
-
-    while !visited.contains_key(&(TARGET.0, TARGET.1, Tool::Torch)) ||
-                !visited.contains_key(&(TARGET.0, TARGET.1, Tool::Gear)) {
-        //dbg!(current);
-        let (x, y, tool) = current;
-        let mut possible_nodes = vec![(x + 1, y), (x, y+1)];
-        if x > 0 {
-            possible_nodes.push((x - 1, y));
-        }
-        if y > 0 {
-            possible_nodes.push((x, y - 1));
-        }
-        let base = *dist.get(&current).unwrap();
-        
-
-        for node in possible_nodes {
-            if !visited.contains_key(&(node.0, node.1, tool)) {
-                let next_cave = cave_system[node.1 * 1000 + node.0];
-                let prev_cave = cave_system[y * 1000 + x];
-
-                let next_tool = match (prev_cave, next_cave) {
-                    (Cave::Rocky, Cave::Wet) => Tool::Gear,
-                    (Cave::Wet, Cave::Rocky) => Tool::Gear,
-                    (Cave::Rocky, Cave::Narrow) => Tool::Torch,
-                    (Cave::Narrow, Cave::Rocky) => Tool::Torch,
-                    (Cave::Narrow, Cave::Wet) => Tool::None,
-                    (Cave::Wet, Cave::Narrow) => Tool::None,
-                    _ => tool,
-                };
-
-                let dv = if tool == next_tool {
-                    1
-                } else {
-                    8
-                };
-
-                let entry = dist.entry((node.0, node.1, next_tool)).or_insert((base + dv));
-                if *entry > base + dv {
-                    *entry = base + dv;
-                }
-            }
-        }
-
-        visited.insert(current, (base, tool));
-        dist.remove(&current);
-        current = *dist.iter().min_by_key(|(k, &v)| v + man_dist((k.0, k.1), TARGET)).unwrap().0;
+    if total > p1 {
+        p1 = total;
     }
 
-    // this is fragile and only works on my input, but i can't be bothered to change it
-    let (mut p2, tool) = visited.get(&(TARGET.0, TARGET.1, Tool::Torch)).unwrap();
+    let mut x_map = Map::new();
 
-    if *tool != Tool::Torch {
-        p2 += 7;
+    for b in &bots {
+        let x_min = b.pos.0 + b.pos.1 + b.pos.2 - b.range;
+        let x_max = b.pos.0 + b.pos.1 + b.pos.2 + b.range + 1;
+
+        *x_map.entry(x_min).or_insert(0) += 1;
+        *x_map.entry(x_max).or_insert(0) -= 1;
     }
 
+    
+    let mut running = 0;
+    let mut max = 0;
+    let mut max_start = 0;
+    let mut max_end = 0;
+
+    for (&pos, &v) in &x_map {
+        running += v;
+        if running > max {
+            max = running;
+            max_start = pos;
+        }
+    }
+
+    println!("{}", max);
+
+    let max_end = *x_map.keys().skip_while(|&&v| v <= max_start).next().unwrap();
+    p2 = max_end - 1;
 
     (p1, p2)
 }
